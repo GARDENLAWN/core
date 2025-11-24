e1b7220ded47af8753941777fd688076973a4c09
GARDENLAWN <rpcsky@gmail.com>
2025-11-24 18:53:53 +0100

    Init

--- Cron/Custom.php
+++ Cron/Custom.php
@@ -33,6 +33,11 @@ class Custom
         //ScraperService::saveAutomowJsonData();
         //ScraperService::prepareAutomowJsonData();
 
+        $string = file_get_contents("/var/www/html/magento/app/code/GardenLawn/Core/Configs/export-images.json");
+        $string = json_decode($string);
+        $string = json_encode($string);
+        file_put_contents("/var/www/html/magento/app/code/GardenLawn/Core/Configs/export-images.json", $string);
+
         $string = file_get_contents("/var/www/html/magento/app/code/GardenLawn/Core/Configs/automow_prepared_data.json");
         $string = json_decode($string);
         $string = json_encode($string);
