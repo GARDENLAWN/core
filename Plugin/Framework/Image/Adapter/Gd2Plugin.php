1092b4608bbe65cee527c4b8eb8244c16d30751a
GARDENLAWN <rpcsky@gmail.com>
2025-12-06 12:28:29 +0100

    Init

--- Plugin/Framework/Image/Adapter/Gd2Plugin.php
+++ Plugin/Framework/Image/Adapter/Gd2Plugin.php
@@ -5,6 +5,7 @@ namespace GardenLawn\Core\Plugin\Framework\Image\Adapter;
 
 use Magento\Framework\Image\Adapter\Gd2;
 use ReflectionProperty;
+use ReflectionException;
 
 class Gd2Plugin
 {
@@ -12,18 +13,33 @@ class Gd2Plugin
     {
         if ($destination && str_ends_with(strtolower($destination), '.webp')) {
             // Add WebP support to callbacks
-            $callbacksProperty = new ReflectionProperty(Gd2::class, '_callbacks');
-            $callbacksProperty->setAccessible(true);
-            $callbacks = $callbacksProperty->getValue($subject);
-            if (!isset($callbacks[IMAGETYPE_WEBP])) {
-                $callbacks[IMAGETYPE_WEBP] = ['output' => 'imagewebp', 'create' => 'imagecreatefromwebp'];
-                $callbacksProperty->setValue($subject, $callbacks);
-            }
+            try {
+                $callbacksProperty = new ReflectionProperty(Gd2::class, '_callbacks');
+                $callbacksProperty->setAccessible(true);
+                $callbacks = $callbacksProperty->getValue($subject);
+                if (!isset($callbacks[IMAGETYPE_WEBP])) {
+                    $callbacks[IMAGETYPE_WEBP] = ['output' => 'imagewebp', 'create' => 'imagecreatefromwebp'];
+                    $callbacksProperty->setValue($subject, $callbacks);
+                }
 
-            // Set file type to WebP
-            $fileTypeProperty = new ReflectionProperty(Gd2::class, '_fileType');
-            $fileTypeProperty->setAccessible(true);
-            $fileTypeProperty->setValue($subject, IMAGETYPE_WEBP);
+                // Set file type to WebP
+                $fileTypeProperty = new ReflectionProperty(Gd2::class, '_fileType');
+                $fileTypeProperty->setAccessible(true);
+                $fileTypeProperty->setValue($subject, IMAGETYPE_WEBP);
+
+                // FIX: Convert palette-based images to true color right before saving as WebP
+                $imageHandlerProperty = new ReflectionProperty(Gd2::class, '_imageHandler');
+                $imageHandlerProperty->setAccessible(true);
+                $imageResource = $imageHandlerProperty->getValue($subject);
+
+                if ($imageResource && function_exists('imageistruecolor') && !imageistruecolor($imageResource)) {
+                    imagepalettetotruecolor($imageResource);
+                }
+
+            } catch (ReflectionException $e) {
+                // Silently ignore if reflection fails, to not break core functionality.
+                // A log could be added here if needed.
+            }
         }
 
         return [$destination, $newName];
